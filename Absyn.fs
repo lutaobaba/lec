44da5e629569597e6321e5788b05455ad6b38be4
lutaobaba <44134494+lutaobaba@users.noreply.github.com>
2021-06-27 16:20:08 +0800

    Add files via upload

--- Absyn.fs
+++ Absyn.fs
@@ -38,6 +38,7 @@ and stmt =
   | Expr of expr                     (* Expression statement   e;   *)
   | Return of expr option            (* Return from method          *)
   | Block of stmtordec list          (* Block: grouping and scope   *)
+  | DoWhile of stmt * expr           (* Do While loop               *)//
   // 语句块内部，可以是变量声明 或语句的列表                                                              
 
 and stmtordec =                                                    
---
+++ example/dowhile.c
@@ -0,0 +1,8 @@
+void main(){
+    int a;
+    a=1;
+    do{a=a+1;
+    print a;
+    }
+    while(a<5)
+}
